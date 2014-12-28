f9cb705d43f1dfba89d36e3a9aaa84befb2f7738
esneider <dariosn@gmail.com>
2014-12-28 14:32:10 +0200

    Add createjs

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
+var bird = {
+    v: 0,
+    y: 0,
+};
+
 function init() {
 
     // Make the canvas fill the screen
     var canvas = document.getElementById('mainCanvas');
-
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
     // Create the scene
     var stage  = new createjs.Stage('mainCanvas');
-    var circle = new createjs.Shape();
 
-    // Enable touch gestures
-    createjs.Touch.enable(stage, true);
+    // Create the bird
+    bird = new createjs.Shape();
+    bird.graphics.beginFill('Crimson').drawCircle(0, 0, 50);
+    bird.y = window.innerHeight / 2;
+    bird.x = window.innerWidth / 2;
+    stage.addChild(bird);
 
-    // Start drawing
-    circle.graphics.beginFill('Crimson').drawCircle(0, 0, 50);
-    circle.x = 100;
-    circle.y = 100;
-    stage.addChild(circle);
-
-    // Handle mouse down event
-    stage.addEventListener("stagemousedown", handleClick);
-
+    // createjs.Tween.get(circle).to({});
     // createjs.Tween.get(circle, {loop: true})
         // .to({x: 400}, 1000, createjs.Ease.getPowInOut(4))
         // .to({alpha: 0, y: 75}, 500, createjs.Ease.getPowInOut(2))
@@ -29,10 +28,23 @@ function init() {
         // .to({alpha: 1, y: 100}, 500, createjs.Ease.getPowInOut(2))
         // .to({x: 100}, 800, createjs.Ease.getPowInOut(2));
 
+    // Handle events
+    stage.addEventListener("stagemousedown", handleClick);
+    stage.addEventListener('tick', handleTick);
+
+    // Enable touch gestures
+    createjs.Touch.enable(stage, true);
+
+    // Start animation
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener('tick', stage);
 }
 
 function handleClick() {
     console.log("you clicked");
+}
+
+function handleTick() {
+    // createjs.Tween.
+    // console.log('hey');
 }
